3d6770a54e6d2e5d97eb239af006ff02016e78f0
PepegaSlayer <129308099+PepegaSlayer@users.noreply.github.com>
2023-11-24 19:30:25 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const weatherButton = document.querySelector('.weather-button');
 const infoContainer = document.querySelector('.info-container');
 
+const weatherList = [];
+
 /* Инициализация велеколепной карты HERE */
 
 var platform = new H.service.Platform({
@@ -26,19 +28,35 @@ var behavior = new H.mapevents.Behavior(mapEvents);
 weatherButton.addEventListener('click', function () {
     const latitude = document.querySelector('.latitude').value;
     const longitude = document.querySelector('.longitude').value;
-    map.setCenter({ lat: latitude, lng: longitude });
+    if ((!isNaN(latitude) && !isNaN(longitude)) &&
+        ((latitude >= -90 && latitude <= 90) && (longitude >= -180 && longitude <= 180))) {
+        getNewData({ lat: latitude, lng: longitude })
+        document.querySelector('.error').style.display = "none";
+    } else {
+        document.querySelector('.error').style.display = "flex";
+    }
+   
+   
+});
 
-    fetch('https://api.weatherapi.com/v1/current.json?key=34eba9a155ff4e09bbc85232232311&q=' + latitude + ',' + longitude, {
+function getNewData(coords)
+{
+    makeMarker(coords);
+    fetch('https://api.weatherapi.com/v1/current.json?key=34eba9a155ff4e09bbc85232232311&q=' + coords['lat'] + ',' + coords['lng'], {
         method: 'GET'
     })
         .then(response => response.json())
         .then(data => {
-            infoContainer.insertBefore(getNewWaether(data), infoContainer.childNodes[0]);
+            const newElement = getNewWaether(data);
+            newElement.addEventListener('click', function () {
+                makeMarker(coords);
+            })
+            infoContainer.insertBefore(newElement, infoContainer.childNodes[0]);
         })
         .catch(error => {
             console.log(error);
         });
-});
+}
 
 function getNewWaether(data)
 {
@@ -61,4 +79,10 @@ function getNewWaether(data)
     return newElement;
 }
 
+function makeMarker(coords)
+{
+    map.removeObjects(map.getObjects());
+    map.setCenter(coords);
+    map.addObject(new H.map.Marker(coords));
+}
 
